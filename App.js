f81a5390da03a480a017b356848ff8ea1d186a03
Martín Alejandro Barros <martinbarros86@gmail.com>
2020-06-28 03:10:57 -0300

    add react-native-stylex and shoutem libraries

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import colors from './src/constants/colors'
+import React from "react";
+// import { StyleSheet, Text, View } from 'react-native';
+import colors from "./src/constants/colors";
+import { SafeAreaView, View, Text } from "react-native";
+import tailwind from "tailwind-rn";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Rolling Exchange</Text>
-    </View>
+    <SafeAreaView style={tailwind("h-full")}>
+      <View style={tailwind("pt-12 items-center")}>
+        <View style={tailwind("bg-blue-200 px-3 py-1 rounded-full")}>
+          <Text style={tailwind("text-blue-800 font-semibold")}>
+            Rolling Exchange
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: colors.text
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: colors.background,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   text: {
+//     color: colors.text
+//   }
+// });
